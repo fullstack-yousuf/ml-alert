a325a517793970ab41f4bd9eafcba2442724cb41
Muhammad Yousuf <muhammad.yousuf@cheaptechies.com>
2026-01-16 16:03:05 +0500

    github action set up

--- backend-ml/firebase_service.py
+++ backend-ml/firebase_service.py
@@ -1,18 +1,48 @@
 import firebase_admin
 from firebase_admin import credentials, db
+import json
+import os
+import tempfile
 
-cred = credentials.Certificate("serviceAccountKey.json")
+# Read credentials from ENV (GitHub Secret)
+firebase_creds = os.environ.get("FIREBASE_CREDENTIALS")
+
+if not firebase_creds:
+    raise ValueError("FIREBASE_CREDENTIALS not found in environment variables")
+
+# Write JSON to temp file
+with tempfile.NamedTemporaryFile(delete=False, mode="w", suffix=".json") as f:
+    f.write(firebase_creds)
+    cred_path = f.name
+
+cred = credentials.Certificate(cred_path)
 
 firebase_admin.initialize_app(cred, {
-    "databaseURL": "https://diabetes-monitoring-fyp-default-rtdb.asia-southeast1.firebasedatabase.app"
+    "databaseURL": "https://diabetes-monitoring-fyp-default-rtdb.firebaseio.com/"
 })
 
 def get_latest_reading():
     ref = db.reference("patients/patient_001/vitals")
-    data = ref.get()
-    return data
+    return ref.get()
+
+def send_alert(payload):
+    ref = db.reference("patients/patient_001/alerts")
+    ref.set(payload)
+# import firebase_admin
+# from firebase_admin import credentials, db
+
+# cred = credentials.Certificate("serviceAccountKey.json")
+
+# firebase_admin.initialize_app(cred, {
+#     "databaseURL": "https://diabetes-monitoring-fyp-default-rtdb.asia-southeast1.firebasedatabase.app"
+# })
+
+# def get_latest_reading():
+#     ref = db.reference("patients/patient_001/vitals")
+#     data = ref.get()
+#     return data
 
 
-def send_alert(ml_alert):
-    alert_ref = db.reference("patients/patient_001/alerts")
-    alert_ref.push(ml_alert)
+# def send_alert(ml_alert):
+#     alert_ref = db.reference("patients/patient_001/alerts")
+#     alert_ref.push(ml_alert)
